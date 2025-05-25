f217ff27c3cd528ec492dd0487e1b270c05a6708
Bao Le <levanbao2105@gmail.com>
2025-05-25 23:20:27 +0700

    chore: remove in-memory cache from CacheExtensions

--- code/backend/Extensions/CacheExtensions.cs
+++ code/backend/Extensions/CacheExtensions.cs
@@ -5,7 +5,6 @@
         public static IServiceCollection AddCacheServices(this IServiceCollection services, IConfiguration configuration)
         {
             // Configure distributed cache using Redis
-            services.AddDistributedMemoryCache();
             services.AddStackExchangeRedisCache(options =>
             {
                 options.Configuration = configuration.GetConnectionString("RedisConnection") ?? throw new InvalidOperationException("Redis connection string not found in configuration.");
