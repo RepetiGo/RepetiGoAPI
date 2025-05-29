5d62e73c27bd29a5e6e0dc46cf8ad845e8404c2e
Bao Le <levanbao2105@gmail.com>
2025-05-29 23:05:24 +0700

    fix: add email validation

--- code/backend/Dtos/AuthDtos/RefreshTokenDto.cs
+++ code/backend/Dtos/AuthDtos/RefreshTokenDto.cs
@@ -3,6 +3,7 @@
     public class RefreshTokenDto
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
         [Required]
         public string RefreshToken { get; set; } = string.Empty;
