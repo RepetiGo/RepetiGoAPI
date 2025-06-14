3c40ee53db1b06927e670f75a004d831dec2076c
UIT.23520349 <23520349@gm.uit.edu.vn>
2025-06-14 19:50:19 +0700

    Update ResponseTemplate.cs
    fix email title

--- FlashcardApp.Api/Helpers/ResponseTemplate.cs
+++ FlashcardApp.Api/Helpers/ResponseTemplate.cs
@@ -1006,7 +1006,7 @@
         <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"" style=""max-width: 600px;"">
           <tr>
             <td align=""center"" bgcolor=""#ffffff"" style=""padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf; text-align: center;"">
-  <h1 style=""margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;"">Email verification</h1>
+  <h1 style=""margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;"">Password reset code</h1>
 </td>
 
           </tr>
