dbaa5394a22cece64d9b9c580f1d0686b0a1b387
Bao Le <levanbao2105@gmail.com>
2025-06-24 23:01:42 +0700

    Add settings update functionality to user profile
    - Implemented `UpdateSettings` endpoint in `ProfilesController`.
    - Added `EasyBonus` property to `SettingsResponse`.
    - Updated `IUsersService` interface with `UpdateSettings` method.
    - Developed `UpdateSettings` method in `UsersService` to handle updates.
    - Created `UpdateSettingsRequest` DTO for settings updates.
    - Added `SettingsMappingProfile` for AutoMapper configurations.

---
+++ RepetiGo.Api/Dtos/SettingsDtos/UpdateSettingsRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace RepetiGo.Api.Dtos.SettingsDtos
+{
+    public class UpdateSettingsRequest
+    {
+        public int? NewCardsPerDay { get; set; }
+        public int? MaxReviewsPerDay { get; set; }
+        public double? StartingEasinessFactor { get; set; }
+        public string? LearningSteps { get; set; }
+        public double? GraduatingInterval { get; set; }
+        public double? EasyInterval { get; set; }
+        public string? RelearningSteps { get; set; }
+        public double? MinimumInterval { get; set; }
+        public double? MaximumInterval { get; set; }
+        public double? EasyBonus { get; set; }
+        public double? HardInterval { get; set; }
+        public double? NewInterval { get; set; }
+    }
+}
---
+++ RepetiGo.Api/MappingProfiles/SettingsMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+using RepetiGo.Api.Dtos.SettingsDtos;
+
+namespace RepetiGo.Api.MappingProfiles
+{
+    public class SettingsMappingProfile : Profile
+    {
+        public SettingsMappingProfile()
+        {
+            CreateMap<UpdateSettingsRequest, Settings>();
+            CreateMap<Settings, SettingsResponse>();
+        }
+    }
+}
